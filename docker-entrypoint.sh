541a5e9c4e482ccbe137479f70a077ac58898c10
gadgrandez <gad_grandez@hotmail.com>
2020-06-18 20:45:22 -0500

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if ! [ -f stunnel.conf ] 
+then
+
+cd /etc/stunnel
+
+cat > stunnel.conf <<_EOF_
+foreground = yes
+setuid = stunnel
+setgid = stunnel
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+cert = /etc/stunnel/stunnel.pem
+client = ${CLIENT:-no}
+[${SERVICE}]
+accept = ${ACCEPT}
+connect = ${CONNECT}
+_EOF_
+
+fi
+
+if ! [ -f stunnel.pem ]
+then
+    openssl req -x509 -nodes -newkey rsa:2048 -days 3650 -subj '/CN=stunnel' \
+                -keyout stunnel.pem -out stunnel.pem
+    chmod 600 stunnel.pem
+fi
+
+exec stunnel "$@"
